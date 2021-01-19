53f5196309bb43201f62074477165604c9fd10d8
Daniel Brown <2469283B@student.gla.ac.uk>
2021-01-19 06:12:52 +0000

    half way through chapter 4 content

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Using BASE_DIR to get the absolute path to the templates directory. 
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+
+# Using BASE_DIR to get the absolute path to the static directory
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Placement of the static files 
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+print(TEMPLATE_DIR)
