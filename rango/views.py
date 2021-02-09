70bfcdbf82746230c8d17f4f6c22a334a4314a94
Daniel Brown <2469283B@student.gla.ac.uk>
2021-02-09 20:30:35 +0000

    Chapter 10 COMPLETE

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,41 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+# This retrieves a given cookie for a session that is stored SERVER-SIDE.
+# Helper function for all other cookies. 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# This is a cookie helper function: NOT  a view. It counts the ammount of visits a client makes (max 1 per day).
+# This stores cookies on the server-side with the above helper function, meaning there is no reason to receive 
+# or change a response.
+def visitor_cookie_handler(request): 
+    # We use the get_server_side_cookie helper function to get the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    # the last_visit_cookie tracks the datetime of the last visit. If there isn't one, it is the current datetime.
+    # last_visit_time is the stripped datetime of last_visit_cookie, which is used to calculate the time since last visit.
+    # Check datetime docs for details on how datetime works. 
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit, increment visits cookie and set the session cookie (server-side) 
+    # to make the last visit now. Else, we set the last_visit cookie to the same value it already had. 
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    # finally, update/set the visits cookie. 
+    request.session['visits'] = visits
 
 
 def index(request):
@@ -17,18 +52,27 @@ def index(request):
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes') [:5]
     page_list = Page.objects.order_by('-views') [:5]
+
+    # handle the cookies
+    visitor_cookie_handler(request)
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    return render(request, 'rango/index.html', context=context_dict)
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request,"rango/index.html", context = context_dict)
 
 def about(request):
+    # The about page now shows the visits, but there may be a case when the about page is 
+    # accessed without ever visiting the index, so we should count visits here too. 
+    visitor_cookie_handler(request)
     context_dict = {'boldmessage': 'This tutorial has been put together by 2469283B'}
+    context_dict['visits'] = request.session['visits']
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     return render(request,"rango/about.html", context = context_dict)
 
 def show_category(request, category_name_slug):
